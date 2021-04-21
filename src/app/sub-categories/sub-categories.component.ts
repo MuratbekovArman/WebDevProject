705eccbe18795ce5db9a429d41cc619d30746889
arman1337 <amuratbekov30@gmail.com>
2021-04-21 21:17:58 +0600

    sub cat

--- src/app/sub-categories/sub-categories.component.ts
+++ src/app/sub-categories/sub-categories.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Sub_category} from '../models';
+import {ActivatedRoute} from '@angular/router';
+import {SubCategoryService} from '../sub-category.service';
 
 @Component({
   selector: 'app-sub-categories',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sub-categories.component.css']
 })
 export class SubCategoriesComponent implements OnInit {
+sub_categories!: Sub_category[] |undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private subcatService: SubCategoryService) { }
 
   ngOnInit(): void {
+    this.getSubCategory();
+  }
+
+  getSubCategory() {
+    this.route.paramMap.subscribe((params) => {
+      const id = Number(params.get('category_id'));
+      this.subcatService.getSubcategories(id).subscribe((subCat) => {
+        this.sub_categories = subCat;
+        console.log(subCat);
+      });
+    });
   }
 
 }
