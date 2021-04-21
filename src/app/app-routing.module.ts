41a2d4105c83d103e47663006b9351ed37a4ca39
arman1337 <amuratbekov30@gmail.com>
2021-04-21 10:44:25 +0600

    roots

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from './main/main.component';
+import {CategoryListComponent} from './category-list/category-list.component';
+import {SubCategoriesComponent} from './sub-categories/sub-categories.component';
+import {ProductsComponent} from './products/products.component';
+import {SalesComponent} from './sales/sales.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
 
 const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'categories/', component: CategoryListComponent},
+  {path: 'categories/:category_id/', component: SubCategoriesComponent},
+  {path: 'categories/:category_id/:subcategory_id/', component: ProductsComponent},
+  {path: 'categories/:category_id/:subcategory_id/:product_id/', component: ProductDetailComponent},
+  {path: 'sales/', component: SalesComponent}
 ];
 
 @NgModule({
