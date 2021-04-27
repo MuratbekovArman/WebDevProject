843771cd8dadb72117ef6e88708974fcfbe7b135
Vladlen <vladli2000610@gmail.com>
2021-04-28 01:30:48 +0600

    fixed search

--- FRONT/src/app/search-results/search-results.component.ts
+++ FRONT/src/app/search-results/search-results.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PRODUCTS, SUB_CATEGORIES } from '../fake_db';
-import { Product } from '../models';
+import { Product, Sub_category } from '../models';
 
 import {SearchService} from '../search.service';
+import {SubcategoryService} from '../subcategory.service';
 
 @Component({
   selector: 'app-search-results',
@@ -16,12 +17,34 @@ export class SearchResultsComponent implements OnInit {
   category_id:number;
 
   constructor(private router : Router,
-              private searchService : SearchService) { }
+              private searchService : SearchService,
+              private subcatService: SubcategoryService) { }
 
   ngOnInit(): void {
     this.getResults();
 
 
+  }
+
+  goToProduct(index:string){
+    const result: Product = this.results[index];
+    this.router.navigate(['/categories/' + this.getCategoryId(result.sub_category) + '/' + result.sub_category + '/' + result.id]);
+  }
+
+  getCategoryId(id):string{
+    for(let result of this.results){
+      console.log(result)
+    }
+    console.log(this.results);
+    if(id == undefined)return "0";
+    let sub_category: Sub_category;
+    this.subcatService.getSubcategories(id).subscribe((subCat) => {
+        sub_category = subCat.find((sC)=>{
+          return id == sC.id;
+        })
+      })
+    const category_id : number = sub_category.category_id ;
+    return category_id.toString();
   }
 
   getResults(){
