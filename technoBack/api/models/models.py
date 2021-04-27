7c57e9028017a57716527150597e93e38ea9e1bf
arman1337 <amuratbekov30@gmail.com>
2021-04-27 17:10:37 +0600

    /

--- technoBack/api/models/models.py
+++ technoBack/api/models/models.py
@@ -28,34 +28,33 @@ class Sale(models.Model):
             'name': self.name
         }
 
-class Product(models.Model):
-        name = models.CharField(max_length=200, null=True)
-        price=models.FloatField(max_length=200, null=True)
-        description = models.CharField(max_length=200, null=True)
-        image = models.CharField(max_length=200, null=True)
-        rating=models.FloatField(max_length=200, null=True)
-        sub_category=models.ForeignKey(Sub_category, related_name="products",on_delete=models.CASCADE")
-    
-    def __str__(self):
-        return {
-            'id': self.id,
-            'name': self.name
-        }
-
 
 class Sub_category(models.Model):
     name = models.CharField(max_length=200, null=True)
     image = models.CharField(max_length=200, null=True)
-    category_id=models.AutoField(max_length=200, null=True)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+
     def __str__(self):
         return {
             'id': self.id,
             'name': self.name
         }
+
     class Meta:
         verbose_name = 'Subcategory'
         verbose_name_plural = 'Subcategories'
 
 
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True)
+    image = models.CharField(max_length=200, null=True)
+    rating = models.FloatField(max_length=200, null=True)
+    sub_category = models.ForeignKey(Sub_category, related_name="products", on_delete=models.CASCADE)
 
-    
+    def __str__(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
