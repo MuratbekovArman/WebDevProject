9d459e813b0e9166ac02470a62ea1b50f06e0042
nazrakhmetaikun <62601592+nazrakhmetaikun@users.noreply.github.com>
2021-04-27 17:00:10 +0600

    dobavila modeli na subcategory and product
    пожалуйста проверьте работает ли

--- technoBack/api/models/models.py
+++ technoBack/api/models/models.py
@@ -27,3 +27,35 @@ class Sale(models.Model):
             'id': self.id,
             'name': self.name
         }
+
+class Product(models.Model):
+        name = models.CharField(max_length=200, null=True)
+        price=models.FloatField(max_length=200, null=True)
+        description = models.CharField(max_length=200, null=True)
+        image = models.CharField(max_length=200, null=True)
+        rating=models.FloatField(max_length=200, null=True)
+        sub_category=models.ForeignKey(Sub_category, related_name="products",on_delete=models.CASCADE")
+    
+    def __str__(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+
+class Sub_category(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    image = models.CharField(max_length=200, null=True)
+    category_id=models.AutoField(max_length=200, null=True)
+    def __str__(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+    class Meta:
+        verbose_name = 'Subcategory'
+        verbose_name_plural = 'Subcategories'
+
+
+
+    
