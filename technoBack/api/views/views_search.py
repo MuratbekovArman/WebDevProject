04543b207be7fe062994cef94b42ff5053e37f74
Vladlen <vladli2000610@gmail.com>
2021-04-27 21:22:21 +0600

    Search view

--- technoBack/api/views/views_search.py
+++ technoBack/api/views/views_search.py
@@ -1,9 +1,12 @@
 import json
 
+from django.db.models import Q
 from django.http.response import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from api.models import Product
+
+from api.serializers import ProductSerializer
 
 def test(request, search_request):
 	return HttpResponse("123")
@@ -11,7 +14,6 @@ def test(request, search_request):
 
 def get_needed_products(request, search_request):
 	if request.method == "GET":
-		data = json.load(request)
-		products = Product.objects.all()
-
-		return JsonResponse(products, safe = False)
+		products = Product.objects.filter(Q(name__contains = search_request) | Q(description__contains = search_request))
+		serializer = ProductSerializer(products, many = True)
+		return JsonResponse(serializer.data, safe = False)
